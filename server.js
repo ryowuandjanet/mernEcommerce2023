325fef3d4d9a05af8f3930304665f5e9dc19eb51
ryowuandjanet <ryowu0329@gmail.com>
2024-03-25 18:57:18 +0800

    03.Nodejs Mongodb Connection Ecommece Mern app

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import colors from 'colors';
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import morgan from 'morgan';
+import connectDB from './config/db.js';
 
-dotenv.config({path: ''})
+dotenv.config()
+
+connectDB()
 
 const app = express()
+
+app.use(express.json())
+app.use(morgan('dev'))
 
 app.get("/", (req,res) => {
   res.send("<h1>Welcome to ecommerce app</h1>")
 })
+
 
 const PORT = process.env.PORT || 8080;
 
